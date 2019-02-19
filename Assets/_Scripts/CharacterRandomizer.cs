9903b39f8e851e8e070062bfd2cde4ec968cd196
thienvantran <thvtran98@gmail.com>
2019-02-19 13:12:33 -0800

    first commit of randomize character

---
+++ Assets/_Scripts/CharacterRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    public class CharacterRandomizer : MonoBehaviour
+    {
+        private ThirdPersonCharacter m_Character;
+        private GameObject[] characterArray;
+        private Material material;
+        private Texture[] textureArray;
+        private Texture texture;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_Character = GetComponent<ThirdPersonCharacter>();
+            int characterCount = m_Character.transform.GetChild(0).childCount;
+            characterArray = new GameObject[characterCount];
+            for (int i = 0; i < characterCount; i++)
+            {
+                characterArray[i] = m_Character.transform.GetChild(0).GetChild(i).gameObject;
+            }
+            characterArray[Random.Range(0, characterCount)].gameObject.SetActive(true);
+        }
+    }
+}
