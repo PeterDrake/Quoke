fcf271ba3a7852f1523ea082e2f21f03c0c59281
Anna Lyubinina <anna@lyubinin.com>
2019-02-21 13:58:33 -0800

    debug

---
+++ Assets/_Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+public class ChangeScene : MonoBehaviour
+{
+    private void OnTriggerEnter (Collider sceneTrigger)
+    {
+        //When your Character collides with the box collider
+        if(sceneTrigger.CompareTag("DoorToStreet"))
+        {
+            SceneManager.LoadScene("StreetScene", LoadSceneMode.Additive);
+        }
+    }
+}
