bcb296b385cb0ebd4c83e05aa7a433945c8ca254
Conor Sandoval <csandovalflemming@lclark.edu>
2019-03-05 13:36:43 -0800

    added prefabs for inventory

--- Assets/_Scripts/InventoryPopup.cs
+++ Assets/_Scripts/InventoryPopup.cs
@@ -8,84 +8,53 @@ using UnityEngine.EventSystems;
 
 public class InventoryPopup : MonoBehaviour
 {
-    [SerializeField] private Text[] labels;
-    [SerializeField] private Image[] icons;
-    [SerializeField] private Text curItemLable;
-    //[SerializeField] private Button equip
-    [SerializeField] private Button use;
-    [SerializeField] private string curItem;
-    // Start is called before the first frame update
+   
+    public Image prefab;
+    public Image Inven;
+    //private Image[] slots;
 
     void Start()
     {
 
     }
-    public void Refresh()
+    /// <summary>
+    /// On the opening of athe inventory its set up
+    /// </summary>
+    private void OnEnable()
     {
-        List<string> items = Managers.Inventory.GetItemList();// list of items in player inventory
+        Debug.Log(Managers.Inventory.GetItemList().Count);
+        this.GetList();
 
-        int len = icons.Length;
+    }
+
+    private void OnDisable()
+    {
+
+        Debug.Log("ondiable");
+        int c = Inven.transform.childCount;
+        for (int i = c - 1; i >= 0; i--)
+        {
+            Destroy(Inven.transform.GetChild(i).gameObject);
+        }
+
+
+    }
+
+    private void GetList()
+    {
+        //  int len = Managers.Inventory.GetItemList().Count;
+
+        List<string> items = Managers.Inventory.GetItemList();
+        int len = items.Count;
         for (int i = 0; i < len; i++)
         {
-            if (i < items.Count) //makes sure there is items to disply
-            {   //make the item visible in the inventory popup
-                labels[i].gameObject.SetActive(true);
-                icons[i].gameObject.SetActive(true);
-                string item = items[i];
-
-                //loadin item icons from resources folder @param file path to item texture
-                Sprite sprite = null;// = Resources.Load<Sprite>();
-                icons[i].sprite = sprite;
-                icons[i].SetNativeSize();
-                int icount = Managers.Inventory.GetItemCount(item);
-                string message = "x" + icount;
-                //can put equiped checks here if we want to have equipables
-                labels[i].text = message;
-
-                //event listeners for clicking on icons
-                EventTrigger.Entry enter = new EventTrigger.Entry();
-                enter.eventID = EventTriggerType.PointerClick;
-                enter.callback.AddListener((BaseEventData arg0) => { onItem(item); });
-
-                EventTrigger trigger = icons[i].GetComponent<EventTrigger>();
-                trigger.triggers.Clear();
-                trigger.triggers.Add(enter);
-            }
-            else
-            {
-                icons[i].gameObject.SetActive(false);
-                labels[i].gameObject.SetActive(false);
-            }
+            //slots[i] =
+            Image slot = Instantiate(prefab);
+            //  slots[i] = slot;
+            slot.transform.SetParent(Inven.transform, false);
+            slot.color = Color.cyan;
+            slot.sprite = (Sprite)Resources.Load(items[i], typeof(Sprite));
+            //slot.sprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/popup", typeof(Sprite));
         }
-        if (!items.Contains(curItem))
-        {
-            curItem = null;
-        }
-        if (curItem == null)//if nothing is selcted hide buttons
-        {
-            curItemLable.gameObject.SetActive(false);
-            use.gameObject.SetActive(false);
-        }
-        else
-        {
-            curItemLable.gameObject.SetActive(true);
-            use.gameObject.SetActive(true);
-            curItemLable.text = curItem + ":";
-        }
-    }
-
-
-
-
-    void onItem(string item)
-    {
-        curItem = item;
-        Refresh();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
