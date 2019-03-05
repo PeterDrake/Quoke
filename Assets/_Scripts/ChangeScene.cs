5276f0b6add8b11d946750350417c3136288ed39
Anna Lyubinina <anna@lyubinin.com>
2019-03-05 15:34:31 -0800

    scene change works, and text works

--- Assets/_Scripts/ChangeScene.cs
+++ Assets/_Scripts/ChangeScene.cs
@@ -6,19 +6,27 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
 
-       public GameObject enterText;
-       public string StreetScene;
+    public Canvas message;
+    public string StreetScene;
 
-    // void Start()
-    // {
-    //     enterText.SetActive(false);
-    // }
+     void Start()
+     {
+        message.gameObject.SetActive(false);
+     }
 
-    void OnMouseOver()
+   // void OnMouseOver()
+   void OnTriggerStay()
     {
+        message.gameObject.SetActive(true);
+
+     //   [E] TO ENTER HOUSE
         if (Input.GetKeyDown(KeyCode.E))
         {
             SceneManager.LoadScene(StreetScene);
         }
     }
-}
+    private void OnTriggerExit()
+        {
+        message.gameObject.SetActive(false);
+        }
+    }
