b3be2218016ef94bfebfd33c74fa8f734523a5ab
oswaldhuynh <138213Erazed!>
2019-03-19 14:54:21 -0700

    remove ignored files/folders

---
+++ Assets/_Scripts/Store/ShoppingPopup.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class ShoppingPopup : MonoBehaviour
+{
+   
+    public Image prefab;
+    public Image Inven;
+    //private Image[] slots;
+
+    void Start()
+    {
+
+    }
+    /// <summary>
+    /// On the opening of athe inventory its set up
+    /// </summary>
+    private void OnEnable()
+    {
+        Debug.Log(Managers.Inventory.GetItemList().Count);
+        this.GetList();
+
+    }
+
+    private void OnDisable()
+    {
+
+        Debug.Log("ondiable");
+        int c = Inven.transform.childCount;
+        for (int i = c - 1; i >= 0; i--)
+        {
+            Destroy(Inven.transform.GetChild(i).gameObject);
+        }
+
+
+    }
+
+    private void GetList()
+    {
+        //  int len = Managers.Inventory.GetItemList().Count;
+
+        List<string> items = Managers.Inventory.GetItemList();
+        int len = items.Count;
+        for (int i = 0; i < len; i++)
+        {
+            //slots[i] =
+            Image slot = Instantiate(prefab);
+            //  slots[i] = slot;
+            slot.transform.SetParent(Inven.transform, false);
+            slot.color = Color.cyan;
+            slot.sprite = (Sprite)Resources.Load(items[i], typeof(Sprite));
+            //slot.sprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/popup", typeof(Sprite));
+        }
+    }
+}
