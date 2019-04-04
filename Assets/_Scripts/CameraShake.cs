cbdef83709fde8e34eb3a5a92968583256c7060a
oswaldhuynh <138213Erazed!>
2019-04-04 14:41:28 -0700

    camera shake script

--- Assets/_Scripts/CameraShake.cs
+++ Assets/_Scripts/CameraShake.cs
@@ -8,10 +8,10 @@ public class CameraShake : MonoBehaviour
 	public Transform camTransform;
 	
 	// How long the object should shake for.
-	public float shakeDuration = 0f;
+	public float shakeDuration = 20f;
 	
 	// Amplitude of the shake. A larger value shakes the camera harder.
-	public float shakeAmount = 0.7f;
+	public float shakeAmount = 0.1f;
 	public float decreaseFactor = 1.0f;
 	
 	Vector3 originalPos;
