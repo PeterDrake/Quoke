9c3d43be4e8ce605ec6b1c22e789e8de41adb90c
oswaldhuynh <138213Erazed!>
2019-04-09 15:20:43 -0700

    initiate quake script

---
+++ Assets/_Scripts/InitiateQuake.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitiateQuake : MonoBehaviour
+{
+
+    private int count;
+    private int check;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        check = Random.Range(8, 12);
+        count = Managers.Player.actionCount;
+        if (check >= count)
+        {
+            gameObject.GetComponent<InitiateQuake>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count = Managers.Player.actionCount;
+        if (count >= check)
+        {
+            //Managers.Level.GoToScene("Transition");
+            //gameObject.GetComponent<InitiateQuake>().enabled = false;
+        }
+    }
+}
