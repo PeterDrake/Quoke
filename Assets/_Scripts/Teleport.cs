8cc89b4bfa676cd320fc56d4e9730d1db2ffb4af
oswaldhuynh <138213Erazed!>
2019-04-25 14:33:41 -0700

    teleport locations in street

--- Assets/_Scripts/Teleport.cs
+++ Assets/_Scripts/Teleport.cs
@@ -6,20 +6,30 @@ public class Teleport : MonoBehaviour
 {
 
     private Transform player;
+    private string location;
+    private GameObject tel;
     public GameObject teleport;
     public GameObject quake;
 
+    /**
+     * On awake, sets disables script and picks a random location to teleport to
+     */
     private void Awake()
     {
         gameObject.GetComponent<Teleport>().enabled = false;
+        var n = Random.Range(4, 6);
+        location = "Teleport" + n;
+        tel = teleport.transform.Find(location).gameObject;
     }
 
-    // Start is called before the first frame update
+    /**
+     * On start, teleports player to location set in Awake, and enables Quake script
+     */
     void Start()
     {
-        Managers.Level.GoToScene("HouseInterior");
+        Managers.Level.GoToScene("StreetScene");
         player = gameObject.transform;
-        player.position = teleport.transform.position;
+        player.position = tel.transform.position;
         gameObject.GetComponent<Teleport>().enabled = false;
         quake.GetComponent<Quake>().enabled = true;
     }
