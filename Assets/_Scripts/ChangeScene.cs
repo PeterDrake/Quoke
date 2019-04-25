e64714db7aaba344f0e4e31ef6170999ca431d26
thienvantran <thvtran98@gmail.com>
2019-04-25 13:56:34 -0700

    fixed loadscene delay

--- Assets/_Scripts/ChangeScene.cs
+++ Assets/_Scripts/ChangeScene.cs
@@ -7,37 +7,43 @@ public class ChangeScene : MonoBehaviour
 {
 
     public Canvas message;
+    private bool isTriggered;
     public string scene;
     public string sceneb;
     void Start()
     {
         message.gameObject.SetActive(false);
+        isTriggered = false;
+        Debug.Log(scene);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isTriggered)
+        {
+            if (Managers.quake)
+            {
+                Managers.Level.GoToScene(sceneb);
+
+            }
+            else
+            {
+                Managers.Level.GoToScene(scene);
+
+            }
+        }
     }
 
     // void OnMouseOver()
     void OnTriggerStay()
     {
         message.gameObject.SetActive(true);
-
-        //   [E] TO ENTER HOUSE
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (Managers.quake)
-            {
-                Managers.Level.GoToScene(sceneb);
-                
-            }
-            else
-            {
-                Managers.Level.GoToScene(scene);
-  
-            }
-            //Managers.Level.GoToScene(scene);
-        }
+        isTriggered = true;
     }
     private void OnTriggerExit()
     {
         message.gameObject.SetActive(false);
+        isTriggered = false;
     }
 
   
