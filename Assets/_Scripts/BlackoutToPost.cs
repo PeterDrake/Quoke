b01039756877600d533e078e011aadcb171d80fe
oswaldhuynh <138213Erazed!>
2019-04-30 14:20:01 -0700

    transition into postquake, blackout

---
+++ Assets/_Scripts/BlackoutToPost.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BlackoutToPost : MonoBehaviour
+{
+
+    private string currentScene;
+    
+    private void Awake()
+    {
+        gameObject.GetComponent<BlackoutToPost>().enabled = false;
+        gameObject.transform.Find("BlackScreen").gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.transform.Find("BlackScreen").GetComponent<Image>().color = new Color(0,0,0,0);
+        currentScene = SceneManager.GetActiveScene().name;
+        Invoke("GoBlack", 6);
+    }
+
+    void GoBlack()
+    {
+        gameObject.transform.Find("BlackScreen").gameObject.SetActive(true);
+        gameObject.transform.Find("BlackScreen").GetComponent<Image>().color = new Color(0,0,0,255);
+        if (currentScene == "HouseInterior")
+        {
+            Managers.Level.GoToScene("QuakeHouseInterior");
+        } else if (currentScene == "StreetScene")
+        {
+            Managers.Level.GoToScene("QuakeStreetScene");
+        }
+        Invoke("ToPost",2);
+        
+    }
+
+    void ToPost()
+    {
+        gameObject.transform.Find("BlackScreen").gameObject.SetActive(false);
+        gameObject.GetComponent<BlackoutToPost>().enabled = false;
+    }
+}
