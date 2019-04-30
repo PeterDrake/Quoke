8e16c4d5c5a62686b0f0447e4f32bf196b1cd435
thienvantran <thvtran98@gmail.com>
2019-04-30 13:36:35 -0700

    thanos dies

---
+++ Assets/_Scripts/ContinueScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueScript : MonoBehaviour
+{
+    public void Continue()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
